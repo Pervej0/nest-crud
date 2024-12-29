42ab3f5509c5cd0bc361298be5982a643eff0588
pervej0 <mdparvez222khan@gmail.com>
2024-12-29 17:05:43 +0600

    project started

---
+++ src/module/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { GetUsers } from '../decorator/user.decorator';
+
+@Controller('user')
+export class UserController {
+  constructor() {}
+  @Get('users')
+  getUsers(@GetUsers() user: any) {
+    console.log(user);
+  }
+}
