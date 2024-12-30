6a379a1b9879e8ba44ea76fec786459b79e6f3f5
pervej0 <mdparvez222khan@gmail.com>
2024-12-31 00:35:44 +0600

    project updated

--- prisma/migrations/20241230162447_init_cloud_db/migration.sql
+++ prisma/migrations/20241230162447_init_cloud_db/migration.sql
@@ -1,15 +1,14 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
-    `fullName` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `phone` INTEGER NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `country` VARCHAR(100) NOT NULL,
     `isStudent` BOOLEAN NOT NULL,
-    `address` VARCHAR(191) NOT NULL,
-    `country` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
--- src/strategy/strategy.auth.ts
+++ src/strategy/strategy.auth.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { User } from '@prisma/client';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismadbService } from 'src/prismadb/prismadb.service';
 
@@ -20,7 +19,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   async validate(payload: { userId: number; email: string }) {
     const user = (await this.prisma.user.findUnique({
       where: { email: payload.email },
-    })) as Partial<User>;
+    })) as Partial<any>;
 
     if (!user) {
       return null;
